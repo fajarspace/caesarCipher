4238cd243252006eb425b0ee3e41a35550c2a5af
Fajar Agung <fajarspace@Fajars-MacBook-Pro.local>
2022-09-30 21:12:29 +0700

    Edit var for key

--- cipher.py
+++ cipher.py
@@ -5,18 +5,21 @@ print('')
 plaintext = str(input('Plain Text: '))
 ciphertext = []
 ascii_code = []
-print('')
+print('') 
+
+geser = int(input('mau geser brp?: '))
 
 #encrypt
 print('ENCRYPT :')
 for i in plaintext:
-    temp = ord(i) + 3
+    temp = ord(i) + geser
     ciphertext.append(chr(temp))
 print('Cipher Text:', ''.join(ciphertext))
 #ascii
 for i in ciphertext[::-1]:  
     ascii_code.append(ord(i))
-print('ASCII code:', ascii_code, '\n')
+print('ASCII code:', ascii_code)
+print ("geser : ",geser,'\n')
 
 #Decrypt
 print('DECRYPT :')
@@ -27,8 +30,8 @@ for i in key[::-1]:
     decrypt_ascii.append(i)
 print('ASCII code:', decrypt_ascii)
 for i in decrypt_ascii:
-    temp = i - 3
+    temp = i - geser
     decrypt_ciphertext.append(chr(temp))
 print('Plain Text:', ''.join(decrypt_ciphertext))
-
+print ("geser : ",geser)
 
